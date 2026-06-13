02a805ec3c156aa606e1062672a038b64a89c9a9
Abe Macedo <abemacedo@u.boisestate.edu>
2026-06-12 20:43:42 -0600

    Lab Activity is Complete

--- SecondsConverter/SecondsConverter.java
+++ SecondsConverter/SecondsConverter.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 public class SecondsConverter 
 {
 
     public static void main(String[] args) 
     {
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
+        Scanner keyInput = new Scanner(System.in);
+
+        System.out.print("Enter the number of seconds: ");
+        int numSec1 = keyInput.nextInt();
+
+        int numHrs = numSec1 / 3600;
+        int numMin = (numSec1 / 60) - (numHrs * 60);
+        int numSeconds = numSec1 - (numMin * 60) - (numHrs  * 3600);
+
+        System.out.println("Hours: " + numHrs);
+        System.out.println("Minutes: " + numMin);
+        System.out.println("Seconds: " + numSeconds);
+
+        keyInput.close();
 
     }
     
