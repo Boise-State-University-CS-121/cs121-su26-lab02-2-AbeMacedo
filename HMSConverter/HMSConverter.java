081bdf8b4d2b7f46e7f975018331bbaf893b48fc
Abe Macedo <abemacedo@u.boisestate.edu>
2026-06-12 18:57:09 -0600

    HMSConverter.java v1.0

---
+++ HMSConverter/HMSConverter.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class HMSConverter
+ {
+    public static void main(String[] args)
+    {
+        Scanner keyInput = new Scanner(System.in);
+
+        System.out.println("Enter the number of hours: ");
+        int numHours = keyInput.nextInt();
+
+        System.out.println("Enter the number of minutes: ");
+        int numMinutes = keyInput.nextInt();
+
+        System.out.println("Enter the number of seconds: ");
+        int numSeconds = keyInput.nextInt();
+
+        int totalSeconds = ();
+
+
+        System.out.println("Total seconds: " + totalSeconds);
+
+        keyInput.close();
+
+    }
+
+}
