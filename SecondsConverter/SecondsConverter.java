c1ad5b3fbefe914522e4ee3f10695c152a8f6e29
Abe Macedo <abemacedo@u.boisestate.edu>
2026-06-12 19:52:25 -0600

    SecondsConverter.java v1.0

---
+++ SecondsConverter/SecondsConverter.java
@@ -0,0 +1,14 @@
+public class SecondsConverter 
+{
+
+    public static void main(String[] args) 
+    {
+
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+    }
+    
+}
